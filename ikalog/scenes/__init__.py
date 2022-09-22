211986bfc2a5c2ff9a1b359cc725d3a7c8cb041b
Takeshi HASEGAWA <hasegaw@gmail.com>
2022-09-23 07:16:17 +0900

    ikalog/scenes/__init__.py: no longer loading spl1 scene classes

--- ikalog/scenes/__init__.py
+++ ikalog/scenes/__init__.py
@@ -17,31 +17,3 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-
-from .game.start import GameStart
-from .game.go_sign import GameGoSign
-from .game.kill import GameKill
-from .game.kill_combo import GameKillCombo
-from .game.dead import GameDead
-from .game.finish import GameFinish
-from .game.low_ink import GameLowInk
-from .game.timer_icon import GameTimerIcon
-from .game.oob import GameOutOfBound
-from .game.special_weapon import GameSpecialWeapon
-from .game.special_gauge import GameSpecialGauge
-from .game.paint_score_tracker import PaintScoreTracker
-from .game.objective_tracker import ObjectiveTracker
-from .game.splatzone_tracker import SplatzoneTracker
-from .game.ranked_battle_events import GameRankedBattleEvents
-from .game.inklings_tracker import InklingsTracker
-
-from .result_detail import ResultDetail
-from .result_judge import ResultJudge
-from .result_udemae import ResultUdemae
-from .result_gears import ResultGears
-from .result_festa import ResultFesta
-
-from .lobby import Lobby
-
-from .downie import Downie
-from .blank import Blank
